1a33d6c0263df9e4be188fda4931bdea2a567085
ImMiguelP <anarchxzx@gmail.com>
2023-11-05 13:39:03 -0800

    style: add icons to contact section

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -101,28 +101,22 @@ const MobileNav = () => {
                           Contact Me
                         </div>
                         <ul role="list" className="-mx-2 mt-2 space-y-1">
-                          {navContact.map((team) => (
-                            <li key={team.name}>
+                          {navContact.map((contact) => (
+                            <li key={contact.name}>
                               <a
-                                href={team.href}
+                                href={contact.href}
                                 className={classNames(
-                                  team.current
+                                  contact.current
                                     ? "bg-gray-50 text-indigo-600"
                                     : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
                                   "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                 )}
                               >
-                                <span
-                                  className={classNames(
-                                    team.current
-                                      ? "text-indigo-600 border-indigo-600"
-                                      : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
-                                    "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
-                                  )}
-                                >
-                                  {team.initial}
-                                </span>
-                                <span className="truncate">{team.name}</span>
+                                <contact.icon
+                                  className="text-gray-400 group-hover:text-primary h-6 w-6 shrink-0"
+                                  aria-hidden="true"
+                                />
+                                {contact.name}
                               </a>
                             </li>
                           ))}
--- components/NavObjs.tsx
+++ components/NavObjs.tsx
@@ -4,6 +4,11 @@ import {
   FolderIcon,
   HomeIcon,
 } from "@heroicons/react/24/outline";
+import {
+  AiOutlineMail,
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
 
 export const navPages = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
@@ -13,7 +18,13 @@ export const navPages = [
 ];
 
 export const navContact = [
-  { id: 1, name: "Email", href: "#", initial: "E", current: false },
-  { id: 2, name: "GitHub", href: "#", initial: "G", current: false },
-  { id: 3, name: "LinkedIn", href: "#", initial: "L", current: false },
+  { id: 1, name: "Email", href: "#", icon: AiOutlineMail, current: false },
+  { id: 2, name: "GitHub", href: "#", icon: AiOutlineGithub, current: false },
+  {
+    id: 3,
+    name: "LinkedIn",
+    href: "#",
+    icon: AiOutlineLinkedin,
+    current: false,
+  },
 ];
