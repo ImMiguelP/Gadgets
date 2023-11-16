d0210922023c14f96c2e26073cda8a855053a87a
ImMiguelP <anarchxzx@gmail.com>
2023-11-15 21:47:15 -0800

    style: responsive

--- app/weather/components/Hero.tsx
+++ app/weather/components/Hero.tsx
@@ -8,13 +8,15 @@ const Hero = () => {
     "Discover the latest weather updates for any location by simply entering it into our intuitive platform. Uncover not only the current forecast but also explore a comprehensive 7-day outlook, detailed weather information, and a plethora of additional features to keep you informed and prepared. Experience the power of precise weather forecasting at your fingertips.";
 
   return (
-    <div className="flex flex-col xl:flex-row w-full h-[80vh] items-center md:p-6 lg:space-x-6">
+    <div className="flex flex-col xl:flex-row w-full items-center md:p-6 lg:space-x-6 ">
       <div className="flex-grow space-y-6">
-        <h1 className="text-6xl">Your City, Your Weather, Now</h1>
+        <h1 className="text-6xl">Your City Your Weather Now!</h1>
         <h2 className="text-md text-secondary-foreground/40">{desc}</h2>
-        <Button href="/weather" className="text-lg rounded-xl">
-          Get Started Now
-        </Button>
+        <div className="w-full flex justify-center xl:justify-start">
+          <Button href="/weather" className="text-lg rounded-xl">
+            Get Started Now
+          </Button>
+        </div>
       </div>
       <div className="flex-grow">
         <Image
