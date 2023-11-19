2357a4a40afbecb45b4d1a80ad0bdd6d1134627f
ImMiguelP <anarchxzx@gmail.com>
2023-11-18 16:41:36 -0800

    test

--- app/weather/api/forcast/[id]/route.ts
+++ app/weather/api/forcast/[id]/route.ts
@@ -4,12 +4,23 @@ export async function GET(
   req: Request,
   { params }: { params: { id: string } }
 ) {
-  const location = params.id;
-  const DATA_SOURCE_URL = `http://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API_KEY}&q=${location}&days=9&aqi=yes&alerts=yes`;
-  const res = await fetch(DATA_SOURCE_URL);
-  if (!res.ok) {
-    throw new Error(`Error fetching ${DATA_SOURCE_URL}: ${res.statusText}`);
+  const { WEATHER_API_KEY } = process.env;
+  try {
+    const location = params.id;
+    const DATA_SOURCE_URL = `http://api.weatherapi.com/v1/forecast.json?key=${WEATHER_API_KEY}&q=${location}&days=9&aqi=yes&alerts=yes`;
+
+    const res = await fetch(DATA_SOURCE_URL);
+
+    if (!res.ok) {
+      const errorMessage = `Error fetching ${DATA_SOURCE_URL}: ${res.statusText}`;
+      console.error(errorMessage);
+      return NextResponse.error();
+    }
+
+    const data = await res.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("An error occurred in the server-side code:", error);
+    return NextResponse.error();
   }
-  const data = await res.json();
-  return NextResponse.json(data);
 }
