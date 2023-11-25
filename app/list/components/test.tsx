977ce77ad9e276963e1060faf25d83fb1d4f5e46
Miguel <anarchxzx@gmail.com>
2023-11-25 15:36:41 -0800

    chore: remove dropdown

--- app/list/components/test.tsx
+++ app/list/components/test.tsx
@@ -128,35 +128,6 @@ export const columns: ColumnDef<TaskTableProps>[] = [
       <div className="capitalize"> {format(row.getValue("date"), "PPP")}</div>
     ),
   },
-  {
-    id: "actions",
-    enableHiding: false,
-    cell: ({ row }) => {
-      const payment = row.original;
-
-      return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <DotsHorizontalIcon className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Copy payment ID
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      );
-    },
-  },
 ];
 
 const DataTableDemo = ({ tasks }: TaskTableProps) => {
